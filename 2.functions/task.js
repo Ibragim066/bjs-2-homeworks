8e95003605778fc8937ca0760d7c1e0052415a3f
Ibragim <gamemega.ao@gmail.com>
2023-08-07 21:23:42 +0300

    вторая задача выполнена

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -16,19 +16,55 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
+  let maxArg = Math.max(...arr);
+  let minArg = Math.min(...arr);
+  let diffArg = maxArg - minArg;
+  return diffArg;
 
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length == 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      sumEvenElement += arr[i];
+    } else if (arr[i] % 2 != 0) {
+      sumOddElement += arr[i];
+    }
+  }
+  let difEvenOdd = sumEvenElement - sumOddElement;
+  return difEvenOdd;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length == 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      sumEvenElement += arr[i];
+      countEvenElement += 1;
+    }
+  }
+  let avg = sumEvenElement / countEvenElement;
+  return avg;
 }
 
 function makeWork(arrOfArr, func) {
