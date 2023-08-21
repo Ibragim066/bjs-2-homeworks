f2a6dd2d791e133def5c58ad9f6fd40afdbb0a81
Ibragim <gamemega.ao@gmail.com>
2023-08-21 19:06:42 +0300

    Третья задача выполнена

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -98,5 +98,41 @@ class Library {
     }
 }
 
+// Задача 3
 
+class Student {
+    constructor(name) {
+        this.name = name
+        this.marks = {}
+    }
+
+    addMark(mark, subject) {
+        if (mark < 2 || mark > 5) {
+            return
+        }
+
+        if (!this.marks[subject]) {
+            this.marks[subject] = []
+        }
+
+        this.marks[subject].push(mark)
+    }
+
+    getAverageBySubject(subject) {
+        if (!this.marks[subject] || this.marks[subject].length === 0) {
+            return 0
+        }
+
+        return this.marks[subject].reduce((acc, mark) => acc + mark, 0) / this.marks[subject].length;
+    }
+
+    getAverage() {
+        const subjects = Object.keys(this.marks)
+        if (subjects.length === 0) {
+            return 0
+        }
+
+        return subjects.reduce((acc, subject) => acc + this.getAverageBySubject(subject), 0) / subjects.length;
+    }
+}
 
