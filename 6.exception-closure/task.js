b1d605acf21f8642f6f143492b368de68f92edf2
Ibragim <gamemega.ao@gmail.com>
2023-08-25 12:56:49 +0300

    Первая задача выполнена

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿// Задача 1
+function parseCount(value) {
+    const valueParsed = Number.parseFloat(value);;
+    if (Number.isNaN(valueParsed)) {
+        throw new Error("Невалидное значение")
+    }
+    return parseValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error
+    }
+}
